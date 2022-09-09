5678f158fcd04a6451c0b460f747cf5dde3cc7da
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-09-09 15:46:57 +0200

    checkout started

--- src/components/NavBarLogo/navbarLogo.jsx
+++ src/components/NavBarLogo/navbarLogo.jsx
@@ -11,7 +11,9 @@ function Navbarwithlogo() {
   const { currentIconState, cartIconHandler } = useContext(CartContext);
   return (
     <div className="flex justify-between items-center mr-32 ml-32 nav-items border-b border-[#FBAF85] text-white ">
-      <Logo className="h-6"></Logo>
+      <Link to="/">
+        <Logo className="h-6"></Logo>
+      </Link>
       <div className="nav-link flex gap-10 font-extrabold mb-5 mt-7">
         <Link to="/">
           <div className="">HOME</div>
