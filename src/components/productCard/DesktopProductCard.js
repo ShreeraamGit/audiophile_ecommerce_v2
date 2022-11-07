0c152eb7da01bff4d0189f4e42415e55abfcc682
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-11-07 09:32:10 +0100

    desktop check

--- src/components/productCard/DesktopProductCard.js
+++ src/components/productCard/DesktopProductCard.js
@@ -13,11 +13,11 @@ const DesktopProductCard = (props) => {
       }
     >
       <img
-        className="rounded-xl h-[32rem] w-[50%]"
+        className="rounded-xl h-[25rem] w-[40%]"
         alt={productsList.name}
         src={productsList.categoryImage.desktop}
       ></img>
-      <div className="flex flex-col gap-9 w-[50%]">
+      <div className="flex flex-col gap-9 w-[45%]">
         <span className="text-[14px] tracking-[10px] text-[#D87D4A]">
           {productsList.new ? "NEW PRODUCT" : null}
         </span>
