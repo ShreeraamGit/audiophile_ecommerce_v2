10b2d0706648027c168df631d3c35c898a88a72b
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-11-08 17:51:59 +0100

    desktop version ui update

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Features = (props) => {
   const { product } = props;
-
+  const [firstLine, ...rest] = product.features.split("\n");
   return (
     <div className="flex flex-col gap-5">
       <h3 className="text-[24px] leading-[38px] tracking-[2px] font-bold">
         FEATURES
       </h3>
       <p className="text-[15px] leading-[25px] text-justify font-[300] text-[#000000]">
-        {product.features}
+        {firstLine}
+        {rest.map((items) => (
+          <Fragment>
+            <br />
+            {items}
+          </Fragment>
+        ))}
       </p>
     </div>
   );
